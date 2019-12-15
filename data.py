f7f574c2e8b43d252f7943990644d8593e3618d2
Nuno Pereira <nunocgpereira@gmail.com>
2019-12-15 12:04:19 +0000

    Initial commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article One',
+			'body': 'Lorem is dhajdka hjka dhkd ajdka dakshd;',
+			'author': 'Brad Traversy',
+			'create_date': '04-25-2018'
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'Lorem is dhajdka hjka dhkd ajdka dakshd;',
+			'author': 'Brad Traversy',
+			'create_date': '04-25-2018'
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'Lorem is dhajdka hjka dhkd ajdka dakshd;',
+			'author': 'Brad Traversy',
+			'create_date': '04-25-2018'
+		}
+	]
+
+	return articles
